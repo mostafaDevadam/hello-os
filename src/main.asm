7bec9e0dd9387181005c57a618193697f936443e
mostafaDevadam <adam.mostafa@outlook.de>
2026-06-23 00:17:58 +0300

    update simple code

--- src/main.asm
+++ src/main.asm
@@ -2,7 +2,7 @@ org 0x7C00
 bits 16
 
 %define ENDL 0x0D, 0x0A
-
+%define ENDP 0Dh, 0Ah
 
 start:
      jmp main
@@ -33,6 +33,7 @@ puts:
 
 
 
+
 main:
      mov ax, 0
      mov ds, ax
@@ -44,14 +45,58 @@ main:
      mov si, msg_hello
      call puts
 
+     mov si, msg_hallo
+     call puts
 
-    hlt
+     ;mov ax, 0
+     ;mov ds, ax
+     ;mov es, ax
+
+     ;mov ah, 0
+     ;int 16h
+
+     ;mov ah, 0Eh
+     ;int 10h
+
+     mov si, msg_prompt
+     call puts
+
+     .wait:
+          mov ah, 0
+          int 16h
+
+          cmp al, 0Dh
+          je .done
+
+          mov ah, 0Eh
+          int 10h
+
+          
+
+          jmp .wait
+
+     .done:
+           mov si, msg_bye
+           call puts
+           
+
+     
+
+
+
+
+    ;hlt
 
 .halt:
+      hlt
       jmp .halt
 
 
 msg_hello: db 'Hello world!', ENDL, 0
+msg_hallo: db 'Hallo', ENDL, 0
+;msg_bye db 0Dh, 0Ah, 'Bye', 0
+msg_bye db ENDL, 'Bye', 0
+msg_prompt db 'Type something and press Enter:', ENDL, 0
 
 times 510-($-$$) db 0
 dw 0AA55h
