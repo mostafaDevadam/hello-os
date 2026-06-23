34df5b5cf8837e9dc671772c35e1131dcadba70f
mostafaDevadam <adam.mostafa@outlook.de>
2026-06-23 19:45:12 +0300

    update simple code

--- src/main.asm
+++ src/main.asm
@@ -116,22 +116,89 @@ main:
           ;call puts
 
 
+
+
+          ;mov si, buffer
+          ;mov di, cmd_user
           
 
-          
 
-
-
-          mov si, buffer
-          mov di, cmd_user
 
      mov si, msg_line
      call puts
 
 
      .read_cmd:
-              mov si, msg_cmd
-              call puts
+
+
+               mov si, msg_cmd
+               call puts
+
+               mov si, buffer_cmd_input
+               
+
+
+     .wait_cmd:
+          mov ah, 0
+          int 16h
+
+          cmp al, 0Dh
+          je .done_cmd
+
+          mov [si], al
+          inc si
+
+          mov ah, 0x0E
+          mov bh, 0
+          int 10h
+
+          
+
+          jmp .wait_cmd
+
+     .done_cmd:
+           mov byte [si], 0
+
+           mov si, msg_line
+           call puts
+          
+
+           ; check if cmd_user = buffer_cmd_input
+           mov si, buffer_cmd_input
+           call puts
+
+           mov si, buffer_cmd_input
+           mov di, cmd_user
+
+
+     .compare_cmd_loop:
+               mov al, [si]
+               mov bl, [di]
+               cmp al, bl
+               jne .not_equal
+               cmp al, 0
+               je .equal
+               inc si
+               inc di
+               jmp .compare_cmd_loop
+
+     .equal:
+          mov si, msg_match
+          call puts
+          jmp .continue
+
+     .not_equal:
+          mov si, msg_no_match
+          call puts
+
+     .continue:
+          mov si, msg_continue
+          call puts
+
+
+
+
+
 
 
      ; .done_cmd_input
@@ -155,45 +222,13 @@ main:
      
 
 
-          
-
-
-          
-     .compare:
-
-             
-             
-             mov al, [si]
-             mov bl, [di]
-
-             cmp al, bl
-             ;jne .not_ok
-
-             cmp al, 0
-             ;je .ok
-
-             inc si
-             inc di
-             jmp .compare
-
-     .ok:
-         mov si, msg_ok
-         call puts
-         jmp .cleanup
-
-
-     .not_ok:
-            mov si, msg_err
-            call puts
-            jmp .halt 
-
      
      
-     .cleanup:
-          mov ah, 0x01
-          mov ch, 0x20
-          mov cl, 0x00
-          int 0x10
+     ;.cleanup:
+          ;mov ah, 0x01
+          ;mov ch, 0x20
+          ;mov cl, 0x00
+          ;int 0x10
 
 
 .halt:
@@ -203,20 +238,21 @@ main:
 
 msg_hello: db 'Hello world!', ENDL, 0
 msg_hallo: db 'Hallo', ENDL, 0
-;msg_bye db 0Dh, 0Ah, 'Bye', 0
 msg_bye db ENDL, 'Bye', 0
 msg_prompt db ENDL, 'Type something and press Enter:', ENDL, 0
 msg_prompt_username db ENDL, 'Type username: ', ENDL, 0
 msg_saved db ENDL,'Data saved in buffer', ENDL, 0
-msg_ok db ENDL, 'Command OK: ', 0
+;msg_ok db ENDL, 'Command OK: ', 0
 msg_err db ENDL, 'Unknown command', 0 
 msg_cmd db ENDL, 'Type CMD (user):', 0
 msg_line db ENDL, '-------------', ENDL, 0
 
-msg_cmd_ok db ENDL, 'Command OK', 0
-msg_cmd_err db ENDL, 'Unknown command', 0 
+;msg_cmd_ok db ENDL, 'Command OK', 0
+;msg_cmd_err db ENDL, 'Unknown command', 0 
 
-
+msg_match db ENDL,'Match found!', 13, 10, 0
+msg_no_match db 'No match!', 13, 10, 0
+msg_continue db 'you can continue!', 13, 10, 0
 
 
 
@@ -226,7 +262,16 @@ cmd_user db 'user', 0
 cmd_input: times 16 db 0
 cmd_len db 0
 
+buffer_cmd_input: times 16 db 0
+
 buffer: times 16 db 0
+
+
+
+
+%if ($-$$) > 510
+     %error "Boot sector code too large!"
+%endif
 
 times 510-($-$$) db 0
 dw 0AA55h
